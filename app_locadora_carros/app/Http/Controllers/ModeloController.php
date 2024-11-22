6b8bff0614b582b3ebbafaabe0b4713b06736fc2
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-11-21 23:55:20 -0300

    ✨feat: API WebService REST para o Resource Modelo

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+    public $modelo;
+
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return response()->json($this->modelo->all(), 200);
     }
 
     /**
@@ -28,15 +35,34 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+
+        $image = $request->file('imagem');
+        $image_urn = $image->store('imagens/modelos', 'public');    
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $image_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+        return response()->json($modelo, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -52,14 +78,67 @@ class ModeloController extends Controller
      */
     public function update(Request $request, Modelo $modelo)
     {
-        //
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. Recurso pesquisado não existe'], 404);
+        }
+
+        $teste = '';
+
+        if ($request->method() == 'PATCH') {
+
+            //Percorrendo todas as regras definidas no model
+            foreach ($modelo->rules() as $input => $regra) {
+                $teste .= 'input: ' . $input . '| Regra: ' . $regra . '<br>';
+
+                //Coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+
+            return ['msg' => 'Requisição PATCH efetuada', 'teste' => $teste];
+        } else {
+            $request->validate($modelo->rules());
+        }
+
+        //Remove o arquivo antigo caso um novo arquivo tenha sido enviado
+        if ($request->file('imagem')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $image = $request->file('imagem');
+        $image_urn = $image->store('imagens/modelos', 'public');    
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $image_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+        
+        return response()->json($modelo, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. Recurso pesquisado não existe'], 404);
+        }
+
+        //Remove o arquivo antigo caso um novo arquivo tenha sido enviado
+        Storage::disk('public')->delete($modelo->imagem);
+
+        $modelo->delete();
+        return ['msg' => 'Modelo deletado com sucesso!'];
     }
 }
--- app_locadora_carros/app/Models/Modelo.php
+++ app_locadora_carros/app/Models/Modelo.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Modelo extends Model
 {
-    //
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+
+    public function rules() {
+        return [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:modelos,nome,'.$this->id.'|min:3|max:50',
+            'imagem' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_portas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean'
+        ];
+
+        /*
+            1) tabela
+            2) nome da coluna que será pesquisada na tabela
+            3) id do registro que será desconsiderado na pesquisa
+
+        */
+    }
 }
